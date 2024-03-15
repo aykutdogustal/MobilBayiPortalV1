39ff40251e866a3fcee649a7d7a4eada7edc92b4
Aykut Doğuştal <aykutdogustal@hotmail.com>
2024-03-15 21:16:33 +0300

    Mimari klasör yapılandırılması.

---
+++ src/main/java/com/MobilBayiPortal/MobilBayiPortalV1/MobilBayiPortalV1Application.java
@@ -0,0 +1,13 @@
+package com.MobilBayiPortal.MobilBayiPortalV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilBayiPortalV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilBayiPortalV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/MobilBayiPortal/MobilBayiPortalV1/MobilBayiPortalV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MobilBayiPortal.MobilBayiPortalV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilBayiPortalV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
